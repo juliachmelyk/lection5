a5b68c149a06a4fb1da4dcc806a7be36cf27507f
Julia <you@example.com>
2019-02-25 20:51:03 +0200

    added 5 objects

---
+++ script.js
@@ -0,0 +1,31 @@
+const carJulia = {
+color: 'black',
+name: 'Mazda',
+maxSpeed: 156,
+model: 'RX-7'
+};
+
+
+const carVasyl = {
+    
+    color: 'red',
+    name: 'Audi',
+    maxSpeed: 200,
+    model: 'TT'
+  };
+
+  const carAndrew = {
+    color: 'blue',
+    name: 'BMW',
+    maxSpeed: 300,
+    model: 'X5'
+};
+
+const carDima = {
+    color: 'blue',
+    name: 'MyCar',
+    maxSpeed: '220',
+    model:'mustang'
+};
+
+  console.log(car);
