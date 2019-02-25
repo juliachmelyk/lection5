5dcb8c19779fd55c71fccc493b77958f43955888
Julia <you@example.com>
2019-02-25 21:15:22 +0200

    random numbers

--- script.js
+++ script.js
@@ -1,31 +1,7 @@
-const carJulia = {
-color: 'black',
-name: 'Mazda',
-maxSpeed: 156,
-model: 'RX-7'
-};
+const list = [10, 40, 50, 70, 90, 30];
+function randomNumber(array) {
+    const arraysLength = array.length - 1; // -1, бо перший індекс 0 
+    return(Math.random() * arraysLength).toFixed();
+}
+console.log(list[randomNumber(list)], 'index -' + randomNumber(list)); // передали масив ліст в функцію
 
-
-const carVasyl = {
-    
-    color: 'red',
-    name: 'Audi',
-    maxSpeed: 200,
-    model: 'TT'
-  };
-
-  const carAndrew = {
-    color: 'blue',
-    name: 'BMW',
-    maxSpeed: 300,
-    model: 'X5'
-};
-
-const carDima = {
-    color: 'blue',
-    name: 'MyCar',
-    maxSpeed: '220',
-    model:'mustang'
-};
-
-  console.log(car);
