487c371af5afab858f4d9176f2ce506a47c56ebf
Julia <you@example.com>
2019-02-26 17:28:37 +0200

    added object me

--- script.js
+++ script.js
@@ -1,53 +1,22 @@
-const carJulia = {
-color: 'black',
-name: 'Mazda',
-maxSpeed: 156,
-model: 'RX-7'
-};
-
-
-const carVasyl = {
-    
-    color: 'red',
-    name: 'Audi',
-    maxSpeed: 200,
-    model: 'TT'
-  };
-
-  const carAndrew = {
-    color: 'blue',
-    name: 'BMW',
-    maxSpeed: 300,
-    model: 'X5'
-};
-
-const carDima = {
-    color: 'blue',
-    name: 'MyCar',
-    maxSpeed: '220',
-    model:'mustang'
-};
-
-const carOlena = {
-    color: 'white',
-    name: 'audi',
-    maxSpeed: '180',
-    model:'A6'
-};
-
-const objects ={
-    carOlena,
-    carDima,
-    carAndrew,
-    carVasyl,
-    carJulia
+const me ={ 
+age: 24,
+heigh: 168,
+hobby: 'Reading',
+favoriteColor: 'black',
+favoriteAnimal: 'cat',
+education: 'LNMU',
+work: 'QA',
+country: 'Ukraine',
+city: 'Lviv',
+adress: 'Zolota Street',
+favoriteFood: 'pizza',
+favoriteMovie: 'Titanic',
+name: 'Julia',
+surname: 'Chmelyk',
+speakingLanguage: 'Ukrainian',
+eyeColor: 'grey',
+favoriteDrink: 'Coffee',
+favoriteBook: 'Bukvar',
+favoriteShoes: 'Sneakers',
+favoriteClothes: 'jeans'
 }
-  console.log(objects);
-
-  const filtered = objects.filter(function(item) {
-
-    return +item.maxSpeed > 200;
-    /// 
-  });
-
-  console.log(filtered);
