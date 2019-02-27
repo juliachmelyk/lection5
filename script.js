c12684e1cfcd8a1771738cc7d5d66bd1e2ede0c2
Julia <you@example.com>
2019-02-27 16:13:57 +0200

    function for unique elements

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
-const me ={ 
-age: 24,
-heigh: 168,
-hobby: 'Reading',
-favoriteColor: 'black',
-favoriteAnimal: 'cat',
-education: 'LNMU',
-work: 'QA',
-country: 'Ukraine',
-city: 'Lviv',
-adress: 'Zolota Street',
-favoriteFood: 'pizza',
-favoriteMovie: 'Titanic',
-name: 'Julia',
-surname: 'Chmelyk',
-speakingLanguage: 'Ukrainian',
-eyeColor: 'grey',
-favoriteDrink: 'Coffee',
-favoriteBook: 'Bukvar',
-favoriteShoes: 'Sneakers',
-favoriteClothes: 'jeans'
+function unique(arr) {
+  let result = [];
+
+  nextInput:
+    for (let i = 0; i < arr.length; i++) {
+      let str = arr[i]; 
+      for (let j = 0; j < result.length; j++) { 
+        if (result[j] == str) continue nextInput;
+      }
+      result.push(str);
+    }
+
+  return result;
 }
+
+let myArray = [2, 4, 5, 4, 4, 2, 5, 3, 5, 4, 3, 3, 2
+];
+
+console.log( unique(myArray) );
+
