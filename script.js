d87b25b685d69453e4acdc8c22f4a7bb1bc2843c
Julia <you@example.com>
2019-02-27 17:21:46 +0200

    founded positive, negative, maximum and minimum numbers in array

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
-const me ={ 
-age: 24,
-heigh: 168,
-hobby: 'Reading',
-favoriteColor: 'black',
-favoriteAnimal: 'cat',
-education: 'LNMU',
-work: 'QA',
-country: 'Ukraine',
-city: 'Lviv',
-adress: 'Zolota Street',
-favoriteFood: 'pizza',
-favoriteMovie: 'Titanic',
-name: 'Julia',
-surname: 'Chmelyk',
-speakingLanguage: 'Ukrainian',
-eyeColor: 'grey',
-favoriteDrink: 'Coffee',
-favoriteBook: 'Bukvar',
-favoriteShoes: 'Sneakers',
-favoriteClothes: 'jeans'
+// порахувати всі від’ємні та додатні, вивести мінімальний та максимальний елемент
+const myArray =[]
+for (let i = -100; i <= 100; i++){
+    myArray.push(i);
 }
+console.log(myArray);
+
+
+let posCount = 0;
+let negativeCount = 0;
+  myArray.forEach((item) => {
+  if (item < 0) {
+    negativeCount++
+  } else if (item > 0) {
+    posCount++
+  }
+})
+
+console.log("Кількість від\'ємних елементів: " + posCount);
+console.log("Кількість додатніх елементів: " + negativeCount);
+console.log("Максимальний елемент масиву: " + Math.max(...myArray));
+console.log("Мінімальний елемент масиву: " + Math.min(...myArray));
+
